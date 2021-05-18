7967397fac7432216fa01cd1e0d934a58e06d7ee
Charlie Gerard <annecharlotte.gerard@gmail.com>
2021-05-18 17:44:43 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -84,7 +84,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
-    publicPath: "/",
+    publicPath: "./",
   },
   plugins: PLUGINS,
   module: {
